4ed2e11ab107f89c8421cdc956b95fc112e66a9f
Harborth, Allen <allenharborth9835@gmail.com>
2021-08-04 17:25:11 -0500

    javascript psuedo code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,44 @@
+//work address varible
+let workAddress = "";
+//radius varible
+let radius = 50;
+//income varible
+let income = 0;
+//apartment address varible
+let apartmentAddress = "";
+//distance varible
+let distance = 0;
+//mpg varible
+let mpg = 10;
+//other bills array
+let otherBills = {}
 
+//function that handles income input
+function incomeHandler(){
+
+}
+//function that handles apartment input
+function apartmentHandler(){
+
+}
+//funtion that handles gas bill input
+function gasCostHandler(){
+
+}
+//function that handles other bills input
+function addBills(){
+
+}
+//function to load bills
+function loadBills(){
+    
+}
+
+//function that calculate monthly output
+function calculate(){
+
+}
+//function to display apartment listings
+
+//function to display google maps
 
