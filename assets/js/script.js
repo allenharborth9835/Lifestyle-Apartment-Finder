333e10417c3b09437127d713b211bf9a6d6a2293
Harborth, Allen <allenharborth9835@gmail.com>
2021-08-04 21:13:01 -0500

    add to functionality to reflect mvc

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,49 +1,61 @@
 //work address varible
 let workAddress = "";
 //radius varible
-let radius = 50;
+let radius = 20;
 //apartment address varible
 let apartmentAddress = "";
 let apartmentAmount = 0;
 //mpg varible
 let mpg = 10;
+//gas price
+let gasPrice = 0
 
 
 //function that handles income input
 function incomeHandler(){
     event.preventDefault();
     if($("#job-address").val() === ""){
-        alert("must put in income and work address")
+        alert("must put in work address")
+        return;
     }
     workAddress = $("#job-address").val();
     radius = $("#radius").val();
-    console.log(workAddress, income, radius)
+    console.log(workAddress, radius)
     return;
 }
 //function that handles apartment input
 function apartmentHandler(){
     event.preventDefault();
+    if($("#apartment-address").val() === ""||$("#apartment-amount").val() === ""){
+        alert("must put in apartment address and apartment cost")
+        return;
+    }
     apartmentAddress = $("#apartment-address").val();
     apartmentAmount = $("#apartment-amount").val();
+    console.log(apartmentAddress, apartmentAmount)
+    return;
 }
 //funtion that handles gas bill input
 function gasCostHandler(){
     event.preventDefault();
+    if($("#MPG").val() === ""||$("#gas-price").val() === ""){
+        alert("must put in apartment address and apartment cost")
+        return;
+    }
     mpg = $("#MPG").val();
     averagePrice = $("#gas-price").val();
+    gasPrice = ((((radius/mpg) * averagePrice)*2)*20)
+    console.log(mpg, averagePrice,gasPrice)
+    return;
 }
 
 //function to load bills
 function loadBills(){
     
 }
-
-//function that calculate monthly output
-function calculate(){
-
-}
 $("#income-input-btn").on("click", incomeHandler)
-
+$("#apartment-input-btn").on("click", apartmentHandler)
+$("#gas-input-btn").on("click", gasCostHandler)
 //function to display apartment listings
 
 //function to display google maps
