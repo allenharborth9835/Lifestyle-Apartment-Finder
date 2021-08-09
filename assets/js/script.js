07b8def150b3f64e46d710f45b19f6e928b787bc
Harborth, Allen <allenharborth9835@gmail.com>
2021-08-08 20:37:36 -0500

    add some of the api functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,16 +40,17 @@ let apartmentChoice = 0;
 let budgetTracker = {
     workAddress: null,
     workStreetAddress: null,
+    workAreaCode:null,
     workCity: null,
     workState: null,
     radius: null,
     apartmentAddress: null,
     apartmentAmount: null,
     gasCost: 0,
-    workLat: 0,
-    workLng: 0,
-    chosenAptLat: 0,
-    chosenAptLng: 0,
+    workLat: null,
+    workLng: null,
+    AptLat: 0,
+    AptLng: 0,
     mpg: null,
     averagePrice: null,
     commuteDistance: 0,
@@ -62,27 +63,7 @@ $(document).ready(function () {
     $('.modal').modal();
 });
 
-//searches for local storage data
-savedData = localStorage.getItem("budgetTracker");
-if(!(savedData===null)){
-    savedData = JSON.parse(savedData);
-    budgetTracker = savedData;
-    if(!(savedData.workAddress===null)){
-        console.log(budgetTracker.workAddress, budgetTracker.radius);
-        $("#apartment-search").html(`<p>searching for apartments within ${budgetTracker.radius} of ${budgetTracker.workAddress}<p>`);
-        apartmentSearch();
-    }
-    if(!(savedData.apartmentAddress===null)){
-        console.log(budgetTracker.apartmentAddress, budgetTracker.apartmentAmount);
-        $("#apartment-pick").html(`<p>you choose ${budgetTracker.apartmentAddress} at ${budgetTracker.apartmentAmount}$<p>`);
-    }
-    if(!(savedData.mpg===null)){
-        //gasCost = ((((budgetTracker.radius/budgetTracker.mpg) * budgetTracker.averagePrice)*2)*22);
-        console.log(budgetTracker.mpg, budgetTracker.averagePrice, budgetTracker.gasCost);
-        $("#gas-bill").html(`<p>the maximum you'll pay for gas is ${parseInt(budgetTracker.gasCost)}$ a month<p>`);
-        $("#total-cost").html(`<p>the average total price of apartment and gas could be as high as ${parseInt(budgetTracker.apartmentAmount) + parseInt(budgetTracker.gasCost)}$ a month<p>`);
-    }
-}
+
 
 //convert an address srting to a fetch query parameter for geoCode()
 function addressToFetchQueryParam(addressString){
@@ -104,7 +85,7 @@ function distanceMatrix(budgetTracker){
 
     const meterToMile = 1609.34;
      
-    fetch(`https://trueway-matrix.p.rapidapi.com/CalculateDrivingMatrix?origins=${budgetTracker.workLat}%2C${budgetTracker.workLng}&destinations=${budgetTracker.chosenAptLat}%2C${budgetTracker.chosenAptLng}`, {
+    fetch(`https://trueway-matrix.p.rapidapi.com/CalculateDrivingMatrix?origins=${budgetTracker.workLat}%2C${budgetTracker.workLng}&destinations=${budgetTracker.AptLat}%2C${budgetTracker.AptLng}`, {
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-key": "1b3e17da97msh8784bd378de9d66p17b153jsn255eb2ee1914",
@@ -112,56 +93,101 @@ function distanceMatrix(budgetTracker){
 	    }
     })
     .then(function(response){
-	return response.json();
+	    return response.json();
 
     })
     .then(function(data){
         console.log(data);
         //for one location coordinate pair {lat, lng}, response is in the form of a 2D array ("distances" in meters or "durations" in seconds) with distance value at position [0][0]
-        budgetTracker.commuteDistance = data.distances[0][0];
-        budgetTracker.commuteTime = data.durations[0][0];
-        console.log("The user commute distance(miles) is: " + Math.round((userGeoData.commuteDistance / meterToMile)));
-        console.log("The user commute time(minutes) is: " + Math.round(userGeoData.commuteTime / 60 ));
+        budgetTracker.commuteDistance = data.distances[0][0]/meterToMile;
+        budgetTracker.commuteTime = data.durations[0][0]/60;
+        console.log("The user commute distance(miles) is: " + Math.round((userGeoData.commuteDistance)));
+        console.log("The user commute time(minutes) is: " + Math.round(userGeoData.commuteTime));
     })
     .catch(err => {
-	console.error(err);
+	    console.error(err);
     });
 }
 
 //function that accepts an address string, then uses the TrueWay Geocoding API to convert the address string into map coordinates {latitude, longitude}
-function geoCode(addressToConvert){
+async function geoCode(addressToConvert){
 
     //place a function here to parse address string and convert it to query format
     convertedQueryParamAddress = addressToFetchQueryParam(addressToConvert);
 
-    fetch(`https://trueway-geocoding.p.rapidapi.com/Geocode?address=${convertedQueryParamAddress}&language=en`, {
+    const fetchResultGeo = await fetch(`https://trueway-geocoding.p.rapidapi.com/Geocode?address=${convertedQueryParamAddress}&language=en`, {
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-key": "1b3e17da97msh8784bd378de9d66p17b153jsn255eb2ee1914",
 		"x-rapidapi-host": "trueway-geocoding.p.rapidapi.com"
 	    }
     })
+    .then(function(response){
+	    return response.json();
+    })
+    .then(function(data){
+    console.log(data);
+    //isolate location coordinates as an object
+    console.log(data.results[0].location);
+    
+    //return location coordinates as an object
+    return data.results[0].location;
+    })
+    .catch(err => {
+	console.error(err);
+    });
+    console.log(fetchResultGeo)
+    return fetchResultGeo;
 
-    //add in loading txt into the html element
+}
 
+//function that accepts an area code, state, city, search radius, and number of desired listings to return from Reality in us API
+async function searchListings(budgetTracker){
+
+    console.log(budgetTracker.workCity);
+
+    //convert city name into a fetch query string
+    queryStringCityName = addressToFetchQueryParam(budgetTracker.workCity);
+
+    console.log(queryStringCityName);
+
+    const searchResponseLimit = 200;
+    
+    //note that we only have 500 API calls per month with this API (hard limit)
+    const fetchResultList = await fetch(`https://realty-in-us.p.rapidapi.com/properties/list-for-rent?state_code=${budgetTracker.workState}&city=${queryStringCityName}&limit=${searchResponseLimit}&offset=0&postal_code=${budgetTracker.workAreaCode}&sort=relevance&radius=${budgetTracker.radius}`, {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "1b3e17da97msh8784bd378de9d66p17b153jsn255eb2ee1914",
+		"x-rapidapi-host": "realty-in-us.p.rapidapi.com"
+	    }
+    })
     .then(function(response){
 	return response.json();
     })
     .then(function(data){
     console.log(data);
-    console.log(data.results[0].location);
 
+    let listingsArray = data.listings
+
+    //return the array of rental listings
+    for(let i = 0; i < listingsArray.length; i++){
+        if(listingsArray[i].price_raw === 0){
+            console.log("Listing #: " + (i+1) + " requires " + "Phone " + listingsArray[i].price + " for price quote");
+        }
+    }
+
+    console.log("We have returned " +data.listings.length + " potential listings according to the requested search parameters");
+    return listingsArray;
+    
     })
     .catch(err => {
 	console.error(err);
     });
+
+    console.log(fetchResultList);
+    return fetchResultList;
 }
 
-function apartmentSearch(){
-    for(let i = 0; i<50;i++){
-        $("#apartment-listings").append(`${i+1}. apartment at  cost  <br>`);
-    }
-}
 
 //function that handles work address input
 function workHandler(){
@@ -171,13 +197,18 @@ function workHandler(){
         return;
     }
     budgetTracker.workStreetAddress = $("#job-street-address").val();
+    budgetTracker.workAreaCode = $("#job-area-code").val();
     budgetTracker.workCity = $("#job-city").val();
     budgetTracker.workState = $("#job-state").val();
     budgetTracker.workAddress = `${budgetTracker.workStreetAddress} ${budgetTracker.workCity}, ${budgetTracker.workState}`;
     budgetTracker.radius = $("#radius").val();
-    
-    localStorage.setItem("budgetTracker", JSON.stringify(budgetTracker));
-    console.log(budgetTracker.workAddress, budgetTracker.radius);
+
+    geoCode(budgetTracker.workAddress).then(function(data){
+        budgetTracker.workLat = data.lat;
+        budgetTracker.workLng = data.lng;
+        localStorage.setItem("budgetTracker", JSON.stringify(budgetTracker));
+    });
+
     $("#apartment-search").html(`<p>searching for apartments within ${budgetTracker.radius} of ${budgetTracker.workAddress}<p>`);
     if(!(budgetTracker.mpg===null)){
         budgetTracker.gasCost = ((((budgetTracker.radius/budgetTracker.mpg) * budgetTracker.averagePrice)*2)*22);
@@ -185,7 +216,25 @@ function workHandler(){
         $("#gas-bill").html(`<p>the maximum you'll pay for gas is ${parseInt(budgetTracker.gasCost)}$ a month<p>`);
         $("#total-cost").html(`<p>the average total price of apartment and gas could be as high as ${parseInt(budgetTracker.apartmentAmount) + parseInt(budgetTracker.gasCost)}$ a month<p>`);
     }
-    apartmentSearch();
+    searchListings(budgetTracker).then(function(data){
+        console.log(data);
+        var divEl = document.querySelector("#apartment-listings");
+        var orderedListEL = document.createElement("ul");
+        divEl.appendChild(orderedListEL);
+        for(let i = 0; i < data.length; i++ ){
+            var orderedListItem = document.createElement("li");
+            if(data[i].photo_count > 0){
+                orderedListItem.innerHTML = "<p>" + (i+1)+". " + data[i].address + "<p>" + "<p>" + data[i].lat + "/" + data[i].lon + "<p>" + "<img src='" 
+                + data[i].photo + "'style='width:autopx;height:autopx;'>" + "<p>" + data[i].sqft + ", Beds: " + data[i].beds + ", Baths: " + data[i].baths 
+                + " Monthly Rent:" + data[i].price + "," + data[i].price_raw + "<p>";
+            } else{
+                orderedListItem.innerHTML = "<p>" + data[i].address + data[i].lat + "/" + data[i].lon + "<p>" + "<img src='" 
+                + "<p>No Photo to Display<h3>" + "'>" + "<p>" +  data[i].sqft + ", Beds: " + data[i].beds + ", Baths: " + data[i].baths 
+                + " Monthly Rent:" + data[i].price + "," + data[i].price_raw + "<p>";
+            }
+            orderedListEL.appendChild(orderedListItem);
+        }
+    });
     return;
 }
 
@@ -200,6 +249,7 @@ function apartmentHandler(){
         return;
     }
     apartmentChoice = $("#apartment-choice").val();
+
     localStorage.setItem("budgetTracker", JSON.stringify(budgetTracker));
 
     $("#apartment-pick").html(`<p>you choose ${budgetTracker.apartmentAddress} at ${budgetTracker.apartmentAmount}$<p>`);
@@ -229,6 +279,41 @@ function gasCostHandler(){
     return;
 }
 
+//searches for local storage data
+savedData = localStorage.getItem("budgetTracker");
+if(!(savedData===null)){
+    savedData = JSON.parse(savedData);
+    budgetTracker = savedData;
+    if(!(savedData.workAddress===null)){
+        $("#apartment-search").html(`<p>searching for apartments within ${budgetTracker.radius} of ${budgetTracker.workAddress}<p>`);
+        searchListings(budgetTracker).then(function(data){
+            console.log(data);
+            var divEl = document.querySelector("#apartment-listings");
+            var orderedListEL = document.createElement("ul");
+            divEl.appendChild(orderedListEL);
+            for(let i = 0; i < data.length; i++ ){
+                var orderedListItem = document.createElement("li");
+                if(data[i].photo_count > 0){
+                    orderedListItem.innerHTML = "<p>" + (i+1)+". " + data[i].address + "<p>" + "<p>" + data[i].lat + "/" + data[i].lon + "<p>" + "<img src='" 
+                    + data[i].photo + "'style='width:autopx;height:autopx;' width='900' height='600'>" + "<p>" + data[i].sqft + ", Beds: " + data[i].beds + ", Baths: " + data[i].baths 
+                    + " Monthly Rent:" + data[i].price + "," + data[i].price_raw + "<p>";
+                } else{
+                    orderedListItem.innerHTML = "<p>" + data[i].address + data[i].lat + "/" + data[i].lon + "<p>" + "<img src='" 
+                    + "<p>No Photo to Display<h3>" + "'>" + "<p>" +  data[i].sqft + ", Beds: " + data[i].beds + ", Baths: " + data[i].baths 
+                    + " Monthly Rent:" + data[i].price + "," + data[i].price_raw + "<p>";
+                }
+                orderedListEL.appendChild(orderedListItem);
+            }
+        });
+    }
+    if(!(savedData.apartmentAddress===null)){
+        $("#apartment-pick").html(`<p>you choose ${budgetTracker.apartmentAddress} at ${budgetTracker.apartmentAmount}$<p>`);
+    }
+    if(!(savedData.mpg===null)){
+        $("#gas-bill").html(`<p>the maximum you'll pay for gas is ${parseInt(budgetTracker.gasCost)}$ a month<p>`);
+        $("#total-cost").html(`<p>the average total price of apartment and gas could be as high as ${parseInt(budgetTracker.apartmentAmount) + parseInt(budgetTracker.gasCost)}$ a month<p>`);
+    }
+}
 
 $("#work-input-btn").on("click", workHandler);
 $("#apartment-input-btn").on("click", apartmentHandler);
